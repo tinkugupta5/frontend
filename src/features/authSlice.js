c5e0347425420e2a2f7cf9b246f58ee442f15ef3
tinkugupta5 <tinku.gupta4@gmail.com>
2024-02-19 00:49:39 +0530

    adding getMe

--- src/features/authSlice.js
+++ src/features/authSlice.js
@@ -26,6 +26,18 @@ export const LoginUser = createAsyncThunk("user/loginUser",async(user,thunkAPI) 
     }
 })
 
+export const getMe = createAsyncThunk("user/getMe", async(_, thunkAPI) => {
+    try {
+        const response = await axios.get('http://localhost:5000/me');
+        return response.data;
+    } catch (error) {
+        if(error.response){
+            const message = error.response.data.msg;
+            return thunkAPI.rejectWithValue(message);
+        }
+    }
+});
+
 export const authSlice = createSlice({
     name:'auth',
     initialState,
@@ -36,6 +48,13 @@ export const authSlice = createSlice({
     extraReducers:(builder) => {
         builder.addCase(LoginUser.pending,(state) => {
             state.isLoading = true;           
-        })
+        });
+
+        builder.addCase(LoginUser.fulfilled, (state, action) =>{
+            state.isLoading = false;
+            state.isSuccess = true;
+            state.user = action.payload;
+        });
+        
     }
 })
